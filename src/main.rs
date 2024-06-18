65fc9c938b495638622be492efdb7a2e674c7615
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-06-18 13:01:58 -0400

    Read File Inputted to Compiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // There should only be two arguments, the first one is the name of the program and the second one is the file name
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let filename = &args[1];
+
+    let contents = std::fs::read_to_string(filename).expect("Something went wrong reading the file");
 }
